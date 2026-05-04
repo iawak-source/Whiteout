f7b47288ef20807254ad8088e635c6b1848bf36f
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-05-04 22:03:14 +0700

    One Last Test 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,5 @@ public class PlayerController : MonoBehaviour
   
     void Update()
     {
-        Vector2 moveVector;
-        moveVector = moveAction.ReadValue<Vector2>();
-        print(moveVector);
     }
 }
