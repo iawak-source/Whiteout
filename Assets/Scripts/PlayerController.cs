4b8287ebfd4a3015f4628b92b7e6ff8ce0ea1d55
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-05-04 21:53:30 +0700

    One Last Test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    InputAction moveAction;
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+  
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        print(moveVector);
+    }
+}
