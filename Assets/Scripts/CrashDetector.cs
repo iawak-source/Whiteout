97e513d59b5f1abee939763ff04c94151a079485
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-05-06 12:09:58 +0700

    Add some effect when player hit the finish line or crash

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1.5f;
+    [SerializeField] ParticleSystem crashParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int LayerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == LayerIndex)
         {
-            SceneManager.LoadScene(0);
+            crashParticles.Play();
+            Invoke("ReloadScene", restartDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,13 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float restartDelay = 1f;
+    [SerializeField] ParticleSystem finishParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if(collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            finishParticles.Play();
+            Invoke("ReLoadScene", restartDelay);
         }
+    }
+    void ReLoadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
