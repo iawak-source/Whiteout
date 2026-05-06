45f9676a4d28a78adf1400c9159042be2149d4bf
Nguyễn Vĩnh Tài <rivenmerryme123@gmail.com>
2026-05-06 13:12:47 +0700

    Add effect when the player slip on the floor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
+
+
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    Vector2 moveVector;
+    SurfaceEffector2D mySurfaceEffector2D;
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        mySurfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
 
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         if (moveVector.x < 0)
         {
@@ -26,5 +37,17 @@ public class PlayerController : MonoBehaviour
             myRigidbody2D.AddTorque(-torqueAmount);
         }
 
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            mySurfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            mySurfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
